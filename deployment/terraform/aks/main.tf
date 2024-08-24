9caa8650e50ffb3fbfd9ddf7c5cb86753ac9d9c1
avinash-nair <82433553+avinash-nair@users.noreply.github.com>
2024-08-24 14:46:13 +1000

    Update main.tf

--- deployment/terraform/aks/main.tf
+++ deployment/terraform/aks/main.tf
@@ -23,7 +23,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   default_node_pool {
     name       = "default"
-    node_count = 1
+    node_count = 3
     vm_size    = "Standard_DS2_v2"
   }
 
